636505123ea4996aa7e056337bffc789f20f70d2
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-09-03 15:45:53 -0300

    Resolução problema 4

--- problem4.php
+++ problem4.php
@@ -9,44 +9,53 @@
  $contadorMaior = 999;
  $contadorMenor = 100;
  $resultado = 0;
- while ($contadorMaior > 99 && $contadorMenor < 1000) {
-    $produto = 0;
-    $produto = $contadorMaior * $contadorMenor;
+ while ($contadorMaior > 99) {
 
-    $primeiraParte = substr($produto,0, 3);
-    $segundaParte = substr($produto, -3);
+    $contadorMenor = 100;
+    while ($contadorMenor < 1000) {
+        $produto = 0;
+        $produto = $contadorMaior * $contadorMenor;
 
-    #var_dump($primeiraParte[1]);
-    #var_dump($segundaParte[1]);
-    #var_dump((intval($primeiraParte[1]) - intval($segundaParte[1])) == 0);
-    #var_dump($produto);
+        $primeiraParte = substr($produto,0, 3);
+        $segundaParte = substr($produto, -3);
 
-    var_dump(strlen($produto) );
+        #var_dump($primeiraParte[1]);
+        #var_dump($segundaParte[1]);
+        #var_dump((intval($primeiraParte[1]) - intval($segundaParte[1])) == 0);
+        #var_dump($produto);
 
-    /*var_dump($primeiraParte[0]);
-    print_r($produto);
-    var_dump($primeiraParte[1]);
-    var_dump(intval($segundaParte[1]));
-    var_dump(intval($primeiraParte[1]) - intval($segundaParte[1]) == 0);
-    exit;*/
+        #var_dump(strlen($produto) );
+        #var_dump($contadorMaior );
+        #var_dump($contadorMenor );
 
-    var_dump((intval($primeiraParte[0]) - intval($segundaParte[2])) == 0);
-    var_dump((intval($primeiraParte[1]) - intval($segundaParte[1])) == 0);
-    var_dump((intval($primeiraParte[2]) - intval($segundaParte[0])) == 0);
-    var_dump($produto);
-    var_dump('################################################################');
+        /*var_dump($primeiraParte[0]);
+        print_r($produto);
+        var_dump($primeiraParte[1]);
+        var_dump(intval($segundaParte[1]));
+        var_dump(intval($primeiraParte[1]) - intval($segundaParte[1]) == 0);
+        exit;*/
+
+        /*var_dump((intval($primeiraParte[0]) - intval($segundaParte[2])) == 0);
+        var_dump((intval($primeiraParte[1]) - intval($segundaParte[1])) == 0);
+        var_dump((intval($primeiraParte[2]) - intval($segundaParte[0])) == 0);
+        var_dump($produto);
+        var_dump('################################################################');*/
 
 
-    if ($produto > $resultado  && strlen($produto) == 6 &&
-        (intval($primeiraParte[0]) - intval($segundaParte[2])) == 0 &&
-        (intval($primeiraParte[1]) - intval($segundaParte[1])) == 0 &&
-        (intval($primeiraParte[2]) - intval($segundaParte[0])) == 0
-    ) {
-        var_dump('entrou');
-        $resultado = $produto;
+        if ($produto > $resultado  && strlen($produto) == 6 &&
+            (intval($primeiraParte[0]) - intval($segundaParte[2])) == 0 &&
+            (intval($primeiraParte[1]) - intval($segundaParte[1])) == 0 &&
+            (intval($primeiraParte[2]) - intval($segundaParte[0])) == 0
+        ) {
+            var_dump($produto);
+            $resultado = $produto;
+        }
+
+        $contadorMenor = $contadorMenor + 1;
     }
+
+    
     $contadorMaior = $contadorMaior - 1;
-    $contadorMenor = $contadorMenor + 1;
  }
 
  echo 'Resultado: ' . $resultado;
